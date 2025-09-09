6d294bb1b0672dcfa5e0e9e3b52c543e487118f3
raihaneka50908 <m.raihanekaputra@gmail.com>
2025-09-09 16:45:35 +0700

    change generate token
    change code to generate new token

--- Case 2/server.py
+++ Case 2/server.py
@@ -5,22 +5,23 @@ import time
 
 app = Flask(__name__)
 
+# Variabel global untuk simpan token
 current_token = None
 
 def refresh_token():
     global current_token
     while True:
         current_token = getJwt()
-        #print("Token diperbarui:", current_token)
-        print("Token Diperbarui")
-        time.sleep(600)  # 600 detik = 10 menit
+        print("Token diperbarui:", current_token)
+        time.sleep(120)  # 600 detik = 10 menit
 
 @app.route("/")
 def index():
-    token = getJwt()
-    return render_template("index.html", token=token)
+    return render_template("index.html", token=current_token)
 
 if __name__ == "__main__":
+    # Buat thread untuk update token setiap 10 menit
     t = threading.Thread(target=refresh_token, daemon=True)
     t.start()
     app.run(debug=True,host='143.198.90.217',port=443,ssl_context=("../../origin.pem","../../private.key"))
+    #app.run(debug=True)
